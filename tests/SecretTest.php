dfdce00ad677b790716da441ec18a3a7dd3c3bed
Ryan Mahoney <ryan@virtuecenter.com>
2014-11-24 11:21:25 -0500

    singleton

--- tests/SecretTest.php
+++ tests/SecretTest.php
@@ -1,11 +1,16 @@
 <?php
 namespace Opine;
+
 use PHPUnit_Framework_TestCase;
+use Opine\Container\Service as Container;
+use Opine\Config\Service as Config;
 
 class SecretTest extends PHPUnit_Framework_TestCase {
+
     public function setup () {
         $root = __DIR__ . '/../public';
-        $container = new Container($root, $root . '/../container.yml');
+        $config = new Config($root);
+        $container = Container::instance($root, $config, $root . '/../container.yml');
     }
 
     public function testSample () {
